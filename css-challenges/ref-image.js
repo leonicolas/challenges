ed6292c8c6f94c8c921e9869fd5b155af6fe3dfc
Leonardo Nicolas <leonicolas@gmail.com>
2023-04-02 22:13:02 -0700

    Add opacity control

--- css-challenges/ref-image.js
+++ css-challenges/ref-image.js
@@ -1,4 +1,3 @@
-
 window.loadRefImage = (imageUrl) => {
 
     function addRefImage(imageUrl) {
@@ -7,20 +6,27 @@ window.loadRefImage = (imageUrl) => {
         <style>
             img.ref-image { position: absolute; display: none; z-index: -999; }
             img.ref-image.show-image { display: unset; }
+            input#ref-image-opacity { position: relative; top: 8px;}
             .ref-image-controls { position: absolute; right: 20px; }
         </style>
         <img class="ref-image" src="${imageUrl}">
-        <span class="ref-image-controls">
-            <label for="show-ref-image">Show Reference Image: </label>
-            <input id="show-ref-image" type="checkbox">
-        </span>`;
+        <div class="ref-image-controls">
+            <label for="ref-image-show">Show Reference Image: </label>
+            <input id="ref-image-show" type="checkbox"><br>
+            <label for="ref-image-opacity">Opacity:</label>
+            <input id="ref-image-opacity" type="range" min="0" max="100" value="100" step="1">
+        </div>`;
         document.body.appendChild(container);
     }
 
     window.addEventListener("load", () => {
         addRefImage(imageUrl);
-        document.querySelector("#show-ref-image").addEventListener("change", () => {
-            document.querySelector("img.ref-image").classList.toggle("show-image");
+        const refImage = document.querySelector("img.ref-image");
+        document.querySelector("#ref-image-show").addEventListener("change", () => {
+            refImage.classList.toggle("show-image");
+        });
+        document.querySelector("#ref-image-opacity").addEventListener("input", (event) => {
+            refImage.style.opacity = parseInt(event.target.value, 10) / 100;
         });
     });
 }
