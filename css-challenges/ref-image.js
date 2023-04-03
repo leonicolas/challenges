25ad311ab30b0cb750b20f563367aa4b7c5e5de7
Leonardo Nicolas <leonicolas@gmail.com>
2023-04-02 22:22:20 -0700

    Make reference image visible by default

--- css-challenges/ref-image.js
+++ css-challenges/ref-image.js
@@ -4,15 +4,15 @@ window.loadRefImage = (imageUrl) => {
         const container = document.createElement("div");
         container.innerHTML = `
         <style>
-            img.ref-image { position: absolute; display: none; z-index: -999; }
-            img.ref-image.show-image { display: unset; }
+            img.ref-image { position: absolute; display: unset; z-index: -999; }
+            img.ref-image.hide-image { display: none; }
             input#ref-image-opacity { position: relative; top: 8px;}
             .ref-image-controls { position: absolute; right: 20px; }
         </style>
         <img class="ref-image" src="${imageUrl}">
         <div class="ref-image-controls">
             <label for="ref-image-show">Show Reference Image: </label>
-            <input id="ref-image-show" type="checkbox"><br>
+            <input id="ref-image-show" type="checkbox" checked><br>
             <label for="ref-image-opacity">Opacity:</label>
             <input id="ref-image-opacity" type="range" min="0" max="100" value="100" step="1">
         </div>`;
@@ -23,7 +23,7 @@ window.loadRefImage = (imageUrl) => {
         addRefImage(imageUrl);
         const refImage = document.querySelector("img.ref-image");
         document.querySelector("#ref-image-show").addEventListener("change", () => {
-            refImage.classList.toggle("show-image");
+            refImage.classList.toggle("hide-image");
         });
         document.querySelector("#ref-image-opacity").addEventListener("input", (event) => {
             refImage.style.opacity = parseInt(event.target.value, 10) / 100;
